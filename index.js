cfbbba2ed964c018d93b0efb29f0b01d8b49f010
EA - Kauê Marques Martins <kaue_marques@educadventista.org>
2026-08-11 11:06:17 -0300

    correções

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const botao = document.querySelector("#theme-toggle");
 
 botao.addEventListener("click", () => {
-    botao.classList.toggle("claro");
+    document.body.classList.toggle("claro");
 });
